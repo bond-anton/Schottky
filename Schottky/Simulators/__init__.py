92a1b2292db16c6270f5b770a8d7d6aa99f167a0
Anton Bondarenko <bond.anton@gmail.com>
2016-07-01 01:01:33 +0300

    Syncing work

--- Schottky/Simulators/__init__.py
+++ Schottky/Simulators/__init__.py
@@ -7,7 +7,7 @@ from ScientificProjects.Client import Client
 class Simulator(object):
 
     def __init__(self, client, name=None, description=None, parts=None,
-                 category_name=None, category_description=None):
+                 category=None, measurement_types=None, measurements=None):
         assert isinstance(client, Client), 'Valid ScientificProjects Client instance is required'
         self.client = client.user_manager
         if name is None:
@@ -22,13 +22,24 @@ class Simulator(object):
                 if isinstance(part, Simulator):
                     self.parts.append(part)
         self.manufacturer = self._create_manufacturer()
-        if category_name is None:
-            category_name = 'Simulation'
-        if category_description is None:
-            category_description = 'Simulation software'
-        self.category = self._create_categories(category_name, category_description)
-        self.equipment = self._register_equipment()
-
+        if category is None:
+            category = {
+                'name': 'Software',
+                'description': 'Measurement, automation, control, simulation, and other software tools',
+                'subcategory': {'name': 'Simulation',
+                                'description': 'Simulation software',
+                                'subcategory': None}}
+        self.equipment = self._register_equipment(category)
+        if measurement_types is None:
+            measurement_types = [{'name': 'Synthetic data',
+                                  'description': 'Pure synthetic data',
+                                  'children': []}]
+        if measurements is None:
+            measurements = [{'name': 'random data generation',
+                             'description': 'generates random data',
+                             'type': 'Synthetic data'}]
+        self.measurements = measurements
+        self._register_measurement_types(measurement_types)
 
     def _create_manufacturer(self):
         name = 'Anton Bondarenko'
@@ -37,21 +48,22 @@ class Simulator(object):
         return self.client.equipment_manager.create_manufacturer(name=name, name_short=name_short,
                                                                  description=description)
 
-    def _create_categories(self, category_name, category_description):
-        description = 'Measurement, automation, control, simulation, and other software tools'
-        root_category = self.client.equipment_manager.create_equipment_category(name='Software',
-                                                                                description=description)
-        return self.client.equipment_manager.create_equipment_category(name=category_name,
-                                                                       description=category_description,
-                                                                       parent=root_category)
+    def _create_categories(self, category, parent=None):
+        if category is None:
+            return parent
+        parent = self.client.equipment_manager.create_equipment_category(name=category['name'],
+                                                                         description=category['description'],
+                                                                         parent=parent)
+        return self._create_categories(category=category['subcategory'], parent=parent)
 
-    def _register_equipment(self):
+    def _register_equipment(self, category):
+        category = self._create_categories(category=category)
         equipment = self.client.equipment_manager.create_equipment(name=self.name,
-                                                                   category=self.category,
+                                                                   category=category,
                                                                    manufacturer=self.manufacturer,
                                                                    description=self.description)
         if self.parts:
-            assembly = self.client.equipment_manager.create_equipment_assembly(name=self.name+'-parts',
+            assembly = self.client.equipment_manager.create_equipment_assembly(name=self.name + '-parts',
                                                                                description='Parts for ' + self.name)
             for part in self.parts:
                 self.client.equipment_manager.add_equipment_to_assembly(assembly=assembly,
@@ -59,15 +71,22 @@ class Simulator(object):
             equipment.assembly = assembly
         return equipment
 
-    def _register_measurement_types(self):
-        name = 'Synthetic data'
-        description = 'Pure synthetic data'
-        measurement_type = self.client.measurement_type_manager.create_measurement_type(name=name,
-                                                                                        description=description,
-                                                                                        parent=None)
+    def _register_measurement_types(self, measurement_types, parent=None):
+        for measurement_type in measurement_types:
+            new_parent = self.client.measurement_type_manager.create_measurement_type(
+                name=measurement_type['name'],
+                description=measurement_type['description'],
+                parent=parent)
+            if measurement_type['children']:
+                self._register_measurement_types(measurement_type['children'], parent=new_parent)
+            else:
+                self.client.equipment_manager.add_measurement_type_to_equipment(self.equipment, new_parent)
+
+    def _register_measurement(self):
+        print(self.name)
 
     def __str__(self):
         description = 'Simulator: %s' % self.name
         description += '\n %s' % description
-        description += '\n Category: %s' % self.category.name
+        description += '\n Category: %s' % self.equipment.category.name
         return description
