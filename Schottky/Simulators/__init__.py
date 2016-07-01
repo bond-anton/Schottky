b2ef59b3c46d6a987cb0d2d3aa1276bef71bc80b
Anton Bondarenko <bond.anton@gmail.com>
2016-07-02 01:09:50 +0300

    Syncing work

--- Schottky/Simulators/__init__.py
+++ Schottky/Simulators/__init__.py
@@ -16,11 +16,11 @@ class Simulator(object):
         if description is None:
             description = 'Simulates properties of corresponding entity'
         self.description = description
-        self.parts = []
+        self.parts = {}
         if isinstance(parts, (list, tuple, np.ndarray)):
             for part in parts:
                 if isinstance(part, Simulator):
-                    self.parts.append(part)
+                    self.parts['name'] = part
         self.manufacturer = self._create_manufacturer()
         if category is None:
             category = {
@@ -65,7 +65,7 @@ class Simulator(object):
         if self.parts:
             assembly = self.client.equipment_manager.create_equipment_assembly(name=self.name + '-parts',
                                                                                description='Parts for ' + self.name)
-            for part in self.parts:
+            for part in self.parts.values():
                 self.client.equipment_manager.add_equipment_to_assembly(assembly=assembly,
                                                                         equipment=part.equipment)
             equipment.assembly = assembly
@@ -82,8 +82,52 @@ class Simulator(object):
             else:
                 self.client.equipment_manager.add_measurement_type_to_equipment(self.equipment, new_parent)
 
-    def _register_measurement(self, measurement_details, samples, parameters, force_new=False):
-        print(self.name)
+    def _register_measurement(self, measurement_details, samples, parameters, input_data=None,
+                              force_new=False):
+        measurement = None
+        no_matches_found = True
+        if not force_new:
+            measurements = self.client.measurement_manager.get_measurements(
+                name=measurement_details['name'])
+            no_matches_found = True
+            matched_measurements = []
+            for measurement in measurements:
+                parameters_match = False
+                samples_match = False
+                input_data_match = False
+                if measurement.parameters == parameters:
+                    parameters_match = True
+                if measurement.samples == samples:
+                    samples_match = True
+                if measurement.input_data == input_data:
+                    input_data_match = True
+                if parameters_match and samples_match and input_data_match:
+                    matched_measurements.append(measurement)
+            if matched_measurements:
+                measurement = matched_measurements[0]
+                for matched_measurement in matched_measurements:
+                    if matched_measurement.progress > measurement.progress:
+                        measurement = matched_measurement
+                no_matches_found = False
+        if force_new or no_matches_found:
+            measurement = self.client.measurement_manager.create_measurement(
+                name=measurement_details['name'],
+                measurement_type=measurement_details['type'],
+                equipment=self.equipment,
+                description=measurement_details['description'])
+            for parameter in parameters:
+                self.client.measurement_manager.add_parameter_to_measurement(
+                    measurement=measurement,
+                    parameter=parameter)
+            for sample in samples:
+                self.client.measurement_manager.add_sample_to_measurement(
+                    measurement=measurement,
+                    sample=sample)
+            if input_data is not None:
+                self.client.measurement_manager.add_input_data_to_measurement(
+                    measurement=measurement,
+                    measurements_collection=input_data)
+        return measurement
 
     def __str__(self):
         description = 'Simulator: %s' % self.name
