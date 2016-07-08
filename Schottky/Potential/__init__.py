bf379d61e16ad6b9d5574b3067ec60a9ce02166a
Anton Bondarenko <bond.anton@gmail.com>
2016-07-08 19:06:19 +0300

    Syncing work.

--- Schottky/Potential/__init__.py
+++ Schottky/Potential/__init__.py
@@ -5,6 +5,8 @@ import numbers
 from Space.Coordinates import transforms as gt
 from Space.Field import Field
 
+from Schottky import constants
+
 
 class UniformField(Field):
     def __init__(self, strength, direction, name='Uniform electric field', field_type='electrostatic'):
@@ -24,7 +26,7 @@ class UniformField(Field):
                 raise ValueError('N-points array shape must be (N, 3)')
         else:
             raise ValueError('at least 3 coordinates are needed for point')
-        return self.strength * distance
+        return -self.strength * distance
 
     def vector_field(self, xyz):
         xyz = np.array(xyz, dtype=np.float)
@@ -42,9 +44,9 @@ class UniformField(Field):
 
 class ChargedSphere(Field):
 
-    def __init__(self, q, r, name='Spherical Coulomb trap', field_type='electrostatic'):
+    def __init__(self, q, r, epsilon=1, name='Spherical Coulomb trap', field_type='electrostatic'):
         self.r = r
-        self.q = q
+        self.q = q / (4 * np.pi * constants['epsilon_0'] * epsilon)
         super(ChargedSphere, self).__init__(name, field_type)
 
     def scalar_field(self, xyz):
@@ -62,19 +64,39 @@ class ChargedSphere(Field):
 
 class ChargedCylinder(Field):
 
-    def __init__(self, l, r, name='Cylindrical Coulomb trap', field_type='electrostatic'):
+    def __init__(self, l, r, epsilon=1, name='Cylindrical Coulomb trap', field_type='electrostatic'):
         self.r = r  # radius of cylinder
-        self.l = l  # linear charge density
+        self.b = constants['q'] * l / (2 * np.pi * constants['epsilon_0'] * epsilon)
         super(ChargedCylinder, self).__init__(name, field_type)
 
     def scalar_field(self, xyz):
         rpz = gt.cartesian_to_cylindrical(xyz)
         rpz[np.where(rpz[:, 0] < self.r), 0] = self.r
-        return self.l * np.log(rpz[:, 0] / self.r)
+        return self.b * np.log(rpz[:, 0] / self.r)
 
     def vector_field(self, xyz):
         field = gt.cartesian_to_cylindrical(xyz)
         field[np.where(field[:, 0] < self.r), 0] = self.r
-        field[:, 0] = self.l / field[:, 0]
+        field[:, 0] = self.b / field[:, 0]
         field[:, 2] = 0
         return gt.cylindrical_to_cartesian(field)
+
+
+class ReciprocalCylinder(Field):
+
+    def __init__(self, a, r, name='1/r Cylindrical potential', field_type='electrostatic'):
+        self.r = r  # radius of cylinder
+        self.a = constants['q'] * a
+        super(ReciprocalCylinder, self).__init__(name, field_type)
+
+    def scalar_field(self, xyz):
+        rpz = gt.cartesian_to_cylindrical(xyz)
+        rpz[np.where(rpz[:, 0] < self.r), 0] = self.r
+        return -self.a / rpz[:, 0]
+
+    def vector_field(self, xyz):
+        field = gt.cartesian_to_cylindrical(xyz)
+        field[np.where(field[:, 0] < self.r), 0] = self.r
+        field[:, 0] = self.a / (field[:, 0]) ** 2
+        field[:, 2] = 0
+        return -1*gt.cylindrical_to_cartesian(field)
--- demo/99_field_demo.py
+++ demo/99_field_demo.py
@@ -2,14 +2,16 @@ from __future__ import division, print_function
 import numpy as np
 from mayavi import mlab
 
-from Schottky.Potential import UniformField, ChargedSphere, ChargedCylinder
+from Schottky.Potential import UniformField, ChargedSphere, ChargedCylinder, ReciprocalCylinder
+from Space.Coordinates import transforms as gt
 from Space.Field import SuperposedField
 import Space_visualization as Visual
 
-
-pos_charged_sphere_field = ChargedSphere(q=1, r=1)
-pos_charged_cylinder_field = ChargedCylinder(l=1, r=1)
-external_field = UniformField(strength=0.25, direction=[0, 1, 0])
+radius = 0.2
+# pos_charged_sphere_field = ChargedSphere(q=1, r=1)
+pos_charged_cylinder_field = ChargedCylinder(l=-3e7, r=radius)
+deformation_cylinder_field = ReciprocalCylinder(a=0.1/1.6e-19, r=radius)
+external_field = UniformField(strength=0.01, direction=[1, 0, 0])
 
 
 fig = mlab.figure('CS demo', bgcolor=(0.0, 0.0, 0.0))  # Create the mayavi figure
@@ -18,15 +20,34 @@ grid = np.mgrid[-10:10:20j, -10:10:20j, -5:5:10j]
 
 superposed_field = SuperposedField('Superposed Field', [
     pos_charged_cylinder_field,
-    pos_charged_sphere_field,
+    deformation_cylinder_field,
     external_field
 ])
 
+'''
 superposed_field_vis = Visual.FieldView(fig, superposed_field,
                                         scalar_field_visible=True,
                                         vector_field_visible=True)
 superposed_field_vis.set_grid(grid)
 superposed_field_vis.set_cs_visible(False)
 superposed_field_vis.draw()
+'''
+
+num_r = 200
+num_phi = 100
+r = np.linspace(0, 10, num=num_r, endpoint=True)
+phi = np.linspace(0, 2*np.pi, num=num_phi, endpoint=True)
+r_grid, p_grid, z_grid = np.meshgrid(r, phi, np.array([0]))
+positions = np.vstack([r_grid.ravel(), p_grid.ravel(), z_grid.ravel()]).T
+xyz = gt.cylindrical_to_cartesian(positions)
+
+scalar_field = superposed_field.scalar_field(xyz).reshape((num_phi, num_r, 1))
+
+mlab.mesh(r_grid[:, :, 0] * np.cos(p_grid[:, :, 0]),
+          r_grid[:, :, 0] * np.sin(p_grid[:, :, 0]),
+          scalar_field[:, :, 0] * 10,
+          representation='wireframe',
+          #colormap='RdBu'
+          )
 
 mlab.show()
