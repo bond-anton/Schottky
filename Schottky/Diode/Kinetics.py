d8815a32893d27a00cb24c3e0ef6b3ab94e4ac91
bond-anton <bond.anton@gmail.com>
2017-02-10 17:08:04 +0300

    Vectorize Poole-Frenkel calculation

--- Schottky/Diode/Kinetics.py
+++ Schottky/Diode/Kinetics.py
@@ -274,8 +274,8 @@ def traps_kinetics(schottky_diode, initial_condition_id, delta_t_min, delta_t_ma
                     #print bl_grid[0,:,0]
                     bl_flat = np.zeros_like(theta)
                     bl_flat[idx] = bl_grid[0,:,0]
-                    #barrier_lowering[:,z_num] = np.array([dopant.trap_potential.barrier_lowering(theta_i)[0] for theta_i in theta])
-                    barrier_lowering[:, z_num] = bl_flat
+                    barrier_lowering[:,z_num] = np.array([dopant.trap_potential.barrier_lowering(theta_i)[0] for theta_i in theta])
+                    #barrier_lowering[:, z_num] = bl_flat
                     #print barrier_lowering[:, z_num]
                     #print bl_flat - barrier_lowering[:, z_num]
                     #poole_frenkel = 0.5 * np.trapz(np.sin(theta) * np.exp(abs(barrier_lowering[:, 0]) / kT), theta)
