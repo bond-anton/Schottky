780f3801ee896443cc6bd30051dde28a0b5d10ef
bond_anton <bond.anton@gmail.com>
2017-02-11 22:45:08 +0300

    Dirty workaround of net-distributed traps Poole-Frenkel effect

--- Schottky/Diode/Kinetics.py
+++ Schottky/Diode/Kinetics.py
@@ -279,9 +279,9 @@ def traps_kinetics(schottky_diode, initial_condition_id, delta_t_min, delta_t_ma
                         r0[idx] = (np.sqrt(loc_a**2 + 4* loc_b * loc_f * np.cos(theta[idx])) - loc_a) / (2 * loc_f * np.cos(theta[idx]))
                         zero_theta_idx = np.where(np.allclose(np.cos(theta), 0))
                         r0[zero_theta_idx] = loc_b / loc_a
-                    non_zero_r_idx = np.where(not np.allclose(r0, 0.0))
+                    non_zero_r_idx = np.where(r0 > 0.0)
                     bl_grid = dopant.trap_potential.potential(r0[non_zero_r_idx], theta[non_zero_r_idx], 0)
-                    #print np.rad2deg(theta[non_zero_r_idx])
+                    print np.rad2deg(theta[non_zero_r_idx])
                     #print bl_grid[0,:,0].shape, theta.shape
                     #print bl_grid[0,:,0]
                     bl_flat = np.zeros_like(theta)
