e6ec5c4f75e9012cb3763413670cbb727b98c399
bond_anton <bond.anton@gmail.com>
2017-02-11 23:41:38 +0300

    Dirty workaround of net-distributed traps Poole-Frenkel effect

--- Schottky/Diode/Kinetics.py
+++ Schottky/Diode/Kinetics.py
@@ -274,6 +274,7 @@ def traps_kinetics(schottky_diode, initial_condition_id, delta_t_min, delta_t_ma
                     if loc_f < 1.0e-1:
                         print 'here', loc_f, z_nodes[z_num]
                         r0[:] = loc_b / loc_a
+                        print len(r0), r0
                     else:
                         idx = np.where(loc_a**2 + 4* loc_b * loc_f * np.cos(theta) >= 0)
                         r0[idx] = (np.sqrt(loc_a**2 + 4* loc_b * loc_f * np.cos(theta[idx])) - loc_a) / (2 * loc_f * np.cos(theta[idx]))
