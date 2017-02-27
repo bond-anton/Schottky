02df818d2530b5a46c3f0a2a4e559d58408cb3a5
bond_anton <bond.anton@gmail.com>
2017-02-27 03:05:52 +0300

    test

--- Schottky/Diode/Kinetics.py
+++ Schottky/Diode/Kinetics.py
@@ -318,13 +318,18 @@ def traps_kinetics(schottky_diode, initial_condition_id, delta_t_min, delta_t_ma
                             pass
                         #print len(r0), r0
                     else:
-                        idx = np.where(loc_a**2 + 4 * loc_b * loc_f * np.cos(theta) >= 0)
+                        determinant = loc_a**2 + 4 * loc_b * loc_f * np.cos(theta)
+                        idx = np.where(determinant >= 0)
                         sqrt = np.sqrt(loc_a**2 + 4 * loc_b * loc_f * np.cos(theta[idx]))
                         sol1 = (-loc_a - sqrt) / (2 * loc_f * np.cos(theta[idx]))
                         sol2 = (-loc_a + sqrt) / (2 * loc_f * np.cos(theta[idx]))
+                        sol1[np.where(sol1 < 0.0)] = 0.0
+                        sol2[np.where(sol2 < 0.0)] = 0.0
                         sol = sol1.copy()
-                        sol2_selection = np.where(sol2 > 0)
-                        #sol2_selection = np.where((sol2 > 0) & (sol2 < sol1))
+                        #sol2_selection = np.where(sol2 > 0)
+                        sol2_selection = np.where((sol2 > 0) & (sol2 < sol1))
+                        sol[sol2_selection] = sol2[sol2_selection]
+                        sol2_selection = np.where((sol2 > 0) & (sol1 <= 0))
                         sol[sol2_selection] = sol2[sol2_selection]
                         r0[idx] = sol
                         #idx = np.where(r0 < loc_b / loc_a / 3)
