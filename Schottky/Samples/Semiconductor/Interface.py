b7a2ea282ba22c48201f7c4cf31394fa25111265
bond_anton <bond.anton@gmail.com>
2017-09-14 02:10:40 +0300

    Interfaces for Semiconductor are ready, no Traps yet :)

--- Schottky/Samples/Semiconductor/Interface.py
+++ Schottky/Samples/Semiconductor/Interface.py
@@ -1,8 +1,7 @@
 from __future__ import division, print_function
-import numbers
+import numpy as np
 
 from Schottky.Samples import Sample
-from Schottky.Samples.Trap import Trap
 
 
 class Interface(Sample):
@@ -12,12 +11,30 @@ class Interface(Sample):
                  traps=None, description=None):
         super(Interface, self).__init__(client=client, name=name, description=description)
         self.load_create_sample()
-        self.depth = None
-        self.smooth_dirac_epsilon = None
+        self.__depth = None
+        self.__smooth_dirac_epsilon = None
         self.traps = []
         self._read_in_depth(depth)
         self._read_in_smooth_dirac_epsilon(smooth_dirac_epsilon)
         self._read_in_traps(traps)
+
+    @property
+    def depth(self):
+        return self.__depth
+
+    @depth.setter
+    def depth(self, depth):
+        try:
+            self.parameters['depth'].float_value = np.float64(depth)
+            self.save_sample_changes()
+        except KeyError:
+            parameter = self.client.parameter_manager.create_numeric_parameter(name='depth',
+                                                                               value=np.float64(depth),
+                                                                               description='Interface depth')
+            self.client.sample_manager.add_parameter_to_sample(sample=self.sample,
+                                                               parameter=parameter)
+            self.reload_parameters()
+        self.__depth = np.float64(depth)
 
     def _read_in_depth(self, depth):
         try:
@@ -25,43 +42,33 @@ class Interface(Sample):
         except KeyError:
             pass
         if self.depth != depth and depth is not None:
-            self.set_depth(depth)
+            self.depth = depth
 
-    def set_depth(self, depth):
-        assert isinstance(depth, numbers.Number), 'Depth must be a number'
-        try:
-            self.parameters['depth'].float_value = float(depth)
-            self.save_sample_changes()
-        except KeyError:
-            parameter = self.client.parameter_manager.create_numeric_parameter(name='depth',
-                                                                               value=float(depth),
-                                                                               description='Interface depth')
-            self.client.sample_manager.add_parameter_to_sample(sample=self.sample,
-                                                               parameter=parameter)
-            self.load_create_sample()
-        self.depth = float(depth)
+    @property
+    def smooth_dirac_epsilon(self):
+        return self.__smooth_dirac_epsilon
 
-    def _read_in_smooth_dirac_epsilon(self, smooth_dirac_epsilon):
+    @smooth_dirac_epsilon.setter
+    def smooth_dirac_epsilon(self, smooth_dirac_epsilon):
         try:
-            self.depth = self.parameters['smooth dirac epsilon'].float_value
-        except KeyError:
-            pass
-        if self.smooth_dirac_epsilon != smooth_dirac_epsilon and smooth_dirac_epsilon is not None:
-            self.set_smooth_dirac_epsilon(smooth_dirac_epsilon)
-
-    def set_smooth_dirac_epsilon(self, smooth_dirac_epsilon):
-        assert isinstance(smooth_dirac_epsilon, numbers.Number), 'Smooth dirac epsilon must be a number'
-        try:
-            self.parameters['smooth dirac epsilon'].float_value = float(smooth_dirac_epsilon)
+            self.parameters['smooth dirac epsilon'].float_value = np.float64(smooth_dirac_epsilon)
             self.save_sample_changes()
         except KeyError:
             parameter = self.client.parameter_manager.create_numeric_parameter(name='smooth dirac epsilon',
-                                                                               value=float(smooth_dirac_epsilon),
+                                                                               value=np.float64(smooth_dirac_epsilon),
                                                                                description='Smooth dirac epsilon')
             self.client.sample_manager.add_parameter_to_sample(sample=self.sample,
                                                                parameter=parameter)
-            self.load_create_sample()
-        self.smooth_dirac_epsilon = float(smooth_dirac_epsilon)
+            self.reload_parameters()
+        self.__smooth_dirac_epsilon = np.float64(smooth_dirac_epsilon)
+
+    def _read_in_smooth_dirac_epsilon(self, smooth_dirac_epsilon):
+        try:
+            self.smooth_dirac_epsilon = self.parameters['smooth dirac epsilon'].float_value
+        except KeyError:
+            pass
+        if self.smooth_dirac_epsilon != smooth_dirac_epsilon and smooth_dirac_epsilon is not None:
+            self.smooth_dirac_epsilon = smooth_dirac_epsilon
 
     def _read_in_traps(self, traps):
         pass
--- Schottky/Samples/Semiconductor/__init__.py
+++ Schottky/Samples/Semiconductor/__init__.py
@@ -1,2 +1,3 @@
 from Schottky.Samples.Semiconductor.Semiconductor import Semiconductor
 from Schottky.Samples.Semiconductor.Dopant import Dopant
+from Schottky.Samples.Semiconductor.Interface import Interface
