f69f451859b95e33d392a4a2a3d34004c0eed80a
bond_anton <bond.anton@gmail.com>
2017-09-14 02:44:44 +0300

    turned setters into @properties

--- Schottky/Samples/Fields/General.py
+++ Schottky/Samples/Fields/General.py
@@ -1,5 +1,4 @@
 from __future__ import division, print_function
-import numpy as np
 
 from BDQuaternions import Rotation
 from BDSpace.Field import Field, SuperposedField
@@ -12,12 +11,31 @@ class SampleField(Sample, Field):
     def __init__(self, client, name, description=None, field_type=None, orientation=None):
         Sample.__init__(self, client=client, name=name, description=description)
         self.load_create_sample()
-        self.field_type = None
+        self.__field_type = None
         self._read_in_field_type(field_type=field_type)
         Field.__init__(self, name=name, field_type=self.field_type)
-        self.orientation = {'origin': [0, 0, 0],
-                            'rotation': [1, 0, 0, 0]}
+        self.__orientation = {'origin': [0, 0, 0],
+                              'rotation': [1, 0, 0, 0]}
         self._read_in_orientation(orientation)
+
+    @property
+    def field_type(self):
+        return self.__field_type
+
+    @field_type.setter
+    def field_type(self, field_type):
+        try:
+            self.parameters['Field type'].string_value = str(field_type)
+            self.save_sample_changes()
+        except KeyError:
+            parameter = self.client.parameter_manager.create_string_parameter(
+                name='Field type',
+                value=str(field_type),
+                description='Field type keyword')
+            self.client.sample_manager.add_parameter_to_sample(sample=self.sample,
+                                                               parameter=parameter)
+            self.reload_parameters()
+        self.__field_type = str(field_type)
 
     def _read_in_field_type(self, field_type):
         try:
@@ -26,67 +44,14 @@ class SampleField(Sample, Field):
             pass
         if field_type is not None:
             if self.field_type != field_type:
-                self.set_field_type(field_type)
+                self.field_type = field_type
 
-    def set_field_type(self, field_type):
-        assert isinstance(field_type, str), 'Field type must be string'
-        try:
-            self.parameters['Field type'].string_value = field_type
-            self.save_sample_changes()
-        except KeyError:
-            parameter = self.client.parameter_manager.create_string_parameter(
-                name='Field type',
-                value=field_type,
-                description='Field type keyword')
-            self.client.sample_manager.add_parameter_to_sample(sample=self.sample,
-                                                               parameter=parameter)
-            self.load_create_sample()
-        self.field_type = field_type
+    @property
+    def orientation(self):
+        return self.__orientation
 
-    def _read_in_orientation(self, orientation):
-        force_set = False
-        try:
-            origin_components = self.parameters['Origin'].children
-            for origin_component in origin_components:
-                if origin_component.name == 'x':
-                    self.orientation['origin'][0] = origin_component.float_value
-                elif origin_component.name == 'y':
-                    self.orientation['origin'][1] = origin_component.float_value
-                elif origin_component.name == 'z':
-                    self.orientation['origin'][2] = origin_component.float_value
-        except KeyError:
-            force_set = True
-            pass
-        try:
-            quaternion_components = self.parameters['Quaternion'].children
-            for quaternion_component in quaternion_components:
-                if quaternion_component.name == 'identity':
-                    self.orientation['rotation'][0] = quaternion_component.float_value
-                elif quaternion_component.name == 'i':
-                    self.orientation['rotation'][1] = quaternion_component.float_value
-                elif quaternion_component.name == 'j':
-                    self.orientation['rotation'][2] = quaternion_component.float_value
-                elif quaternion_component.name == 'k':
-                    self.orientation['rotation'][3] = quaternion_component.float_value
-        except KeyError:
-            force_set = True
-            pass
-        if orientation is not None or force_set:
-            if self.orientation != orientation or force_set:
-                if orientation is None:
-                    orientation = self.orientation
-                self.set_orientation(orientation)
-        else:
-            self._apply_orientation(self.orientation)
-
-    def set_orientation(self, orientation):
-        assert isinstance(orientation, dict), 'Orientation must be dictionary with origin and rotation quaternion'
-        assert 'origin' in orientation.keys(), 'Orientation must be dictionary with origin and rotation quaternion'
-        assert 'rotation' in orientation.keys(), 'Orientation must be dictionary with origin and rotation quaternion'
-        assert isinstance(orientation['origin'], (list, tuple, np.ndarray)), 'Origin must be 3 component iterable'
-        assert isinstance(orientation['rotation'], (list, tuple, np.ndarray)), 'Rotation must be 4 component iterable'
-        assert len(orientation['origin']) == 3, 'Origin must be 3 component iterable'
-        assert len(orientation['rotation']) == 4, 'Rotation must be 4 component iterable'
+    @orientation.setter
+    def orientation(self, orientation):
         quaternion = orientation['rotation']
         origin = orientation['origin']
         try:
@@ -116,7 +81,7 @@ class SampleField(Sample, Field):
                                                                    value=float(origin[2]),
                                                                    description='Z component of origin point',
                                                                    parent=parameter)
-            self.load_create_sample()
+            self.reload_parameters()
         try:
             quaternion_components = self.parameters['Quaternion'].children
             for quaternion_component in quaternion_components:
@@ -150,9 +115,45 @@ class SampleField(Sample, Field):
                                                                    value=float(quaternion[3]),
                                                                    description='k component of quaternion',
                                                                    parent=parameter)
-            self.load_create_sample()
-        self.orientation = orientation
+            self.reload_parameters()
+        self.__orientation = orientation
         self._apply_orientation(self.orientation)
+
+    def _read_in_orientation(self, orientation):
+        force_set = False
+        try:
+            origin_components = self.parameters['Origin'].children
+            for origin_component in origin_components:
+                if origin_component.name == 'x':
+                    self.orientation['origin'][0] = origin_component.float_value
+                elif origin_component.name == 'y':
+                    self.orientation['origin'][1] = origin_component.float_value
+                elif origin_component.name == 'z':
+                    self.orientation['origin'][2] = origin_component.float_value
+        except KeyError:
+            force_set = True
+            pass
+        try:
+            quaternion_components = self.parameters['Quaternion'].children
+            for quaternion_component in quaternion_components:
+                if quaternion_component.name == 'identity':
+                    self.orientation['rotation'][0] = quaternion_component.float_value
+                elif quaternion_component.name == 'i':
+                    self.orientation['rotation'][1] = quaternion_component.float_value
+                elif quaternion_component.name == 'j':
+                    self.orientation['rotation'][2] = quaternion_component.float_value
+                elif quaternion_component.name == 'k':
+                    self.orientation['rotation'][3] = quaternion_component.float_value
+        except KeyError:
+            force_set = True
+            pass
+        if orientation is not None or force_set:
+            if self.orientation != orientation or force_set:
+                if orientation is None:
+                    orientation = self.orientation
+                self.orientation = orientation
+        else:
+            self._apply_orientation(self.orientation)
 
     def _update_orientation(self):
         rotation = Rotation(euler_angles_convention=self.coordinate_system.euler_angles_convention['title'])
@@ -161,7 +162,7 @@ class SampleField(Sample, Field):
         origin = self.coordinate_system.origin
         orientation = {'origin': origin,
                        'rotation': quaternion}
-        self.set_orientation(orientation)
+        self.orientation = orientation
 
     def _apply_orientation(self, orientation):
         rotation = Rotation(quadruple=orientation['rotation'],
@@ -191,38 +192,19 @@ class SuperpositionField(Sample, SuperposedField):
     def __init__(self, client, name, fields=None, description=None, orientation=None):
         Sample.__init__(self, client=client, name=name, description=description, parameters=None)
         self.load_create_sample()
-        self.fields = []
+        self.__fields = []
         self._read_in_fields(fields=fields)
         SuperposedField.__init__(self, name=name, fields=self.fields)
-        self.orientation = {'origin': [0, 0, 0],
-                            'rotation': [1, 0, 0, 0]}
+        self.__orientation = {'origin': [0, 0, 0],
+                              'rotation': [1, 0, 0, 0]}
         self._read_in_orientation(orientation)
 
-    def _read_in_fields(self, fields):
-        try:
-            field_components = self.parameters['Fields'].children
-            for field in field_components:
-                field_module_name, field_class_name = field.string_value.split('::')
-                field_name = field.name
-                field_id = int(field.float_value)
-                field_module = __import__(field_module_name, fromlist=[field_class_name])
-                field_class = getattr(field_module, field_class_name)
-                field_sample = field_class(client=self.client.session_manager, name=field_name)
-                if field_sample.sample.id == field_id:
-                    self.fields.append(field_sample)
-                else:
-                    print('Field IDs do not match')
-        except KeyError:
-            pass
-        if fields is not None:
-            if self.fields != fields:
-                self.set_fields(fields)
+    @property
+    def fields(self):
+        return self.__fields
 
-    def set_fields(self, fields):
-        assert isinstance(fields, (list, tuple)), 'Expected a list of fields'
-        for field in fields:
-            assert isinstance(field, Field), 'Expected a list of fields'
-            assert isinstance(field, Sample), 'Expected a list of fields'
+    @fields.setter
+    def fields(self, fields):
         try:
             parameter = self.parameters['Fields']
             for field in fields:
@@ -259,53 +241,35 @@ class SuperpositionField(Sample, SuperposedField):
                     description=field.description,
                     parent=parameter)
         self.parameters = {}
-        self.load_create_sample()
-        self.fields = fields
+        self.reload_parameters()
+        self.__fields = fields
 
-    def _read_in_orientation(self, orientation):
-        force_set = False
+    def _read_in_fields(self, fields):
         try:
-            origin_components = self.parameters['Origin'].children
-            for origin_component in origin_components:
-                if origin_component.name == 'x':
-                    self.orientation['origin'][0] = origin_component.float_value
-                elif origin_component.name == 'y':
-                    self.orientation['origin'][1] = origin_component.float_value
-                elif origin_component.name == 'z':
-                    self.orientation['origin'][2] = origin_component.float_value
+            field_components = self.parameters['Fields'].children
+            for field in field_components:
+                field_module_name, field_class_name = field.string_value.split('::')
+                field_name = field.name
+                field_id = int(field.float_value)
+                field_module = __import__(field_module_name, fromlist=[field_class_name])
+                field_class = getattr(field_module, field_class_name)
+                field_sample = field_class(client=self.client.session_manager, name=field_name)
+                if field_sample.sample.id == field_id:
+                    self.fields.append(field_sample)
+                else:
+                    print('Field IDs do not match')
         except KeyError:
-            force_set = True
             pass
-        try:
-            quaternion_components = self.parameters['Quaternion'].children
-            for quaternion_component in quaternion_components:
-                if quaternion_component.name == 'identity':
-                    self.orientation['rotation'][0] = quaternion_component.float_value
-                elif quaternion_component.name == 'i':
-                    self.orientation['rotation'][1] = quaternion_component.float_value
-                elif quaternion_component.name == 'j':
-                    self.orientation['rotation'][2] = quaternion_component.float_value
-                elif quaternion_component.name == 'k':
-                    self.orientation['rotation'][3] = quaternion_component.float_value
-        except KeyError:
-            force_set = True
-            pass
-        if orientation is not None or force_set:
-            if self.orientation != orientation or force_set:
-                if orientation is None:
-                    orientation = self.orientation
-                self.set_orientation(orientation)
-        else:
-            self._apply_orientation(self.orientation)
+        if fields is not None:
+            if self.fields != fields:
+                self.fields = fields
 
-    def set_orientation(self, orientation):
-        assert isinstance(orientation, dict), 'Orientation must be dictionary with origin and rotation quaternion'
-        assert 'origin' in orientation.keys(), 'Orientation must be dictionary with origin and rotation quaternion'
-        assert 'rotation' in orientation.keys(), 'Orientation must be dictionary with origin and rotation quaternion'
-        assert isinstance(orientation['origin'], (list, tuple, np.ndarray)), 'Origin must be 3 component iterable'
-        assert isinstance(orientation['rotation'], (list, tuple, np.ndarray)), 'Rotation must be 4 component iterable'
-        assert len(orientation['origin']) == 3, 'Origin must be 3 component iterable'
-        assert len(orientation['rotation']) == 4, 'Rotation must be 4 component iterable'
+    @property
+    def orientation(self):
+        return self.__orientation
+
+    @orientation.setter
+    def orientation(self, orientation):
         quaternion = orientation['rotation']
         origin = orientation['origin']
         try:
@@ -335,7 +299,7 @@ class SuperpositionField(Sample, SuperposedField):
                                                                    value=float(origin[2]),
                                                                    description='Z component of origin point',
                                                                    parent=parameter)
-            self.load_create_sample()
+            self.reload_parameters()
         try:
             quaternion_components = self.parameters['Quaternion'].children
             for quaternion_component in quaternion_components:
@@ -369,9 +333,45 @@ class SuperpositionField(Sample, SuperposedField):
                                                                    value=float(quaternion[3]),
                                                                    description='k component of quaternion',
                                                                    parent=parameter)
-            self.load_create_sample()
-        self.orientation = orientation
+            self.reload_parameters()
+        self.__orientation = orientation
         self._apply_orientation(self.orientation)
+
+    def _read_in_orientation(self, orientation):
+        force_set = False
+        try:
+            origin_components = self.parameters['Origin'].children
+            for origin_component in origin_components:
+                if origin_component.name == 'x':
+                    self.orientation['origin'][0] = origin_component.float_value
+                elif origin_component.name == 'y':
+                    self.orientation['origin'][1] = origin_component.float_value
+                elif origin_component.name == 'z':
+                    self.orientation['origin'][2] = origin_component.float_value
+        except KeyError:
+            force_set = True
+            pass
+        try:
+            quaternion_components = self.parameters['Quaternion'].children
+            for quaternion_component in quaternion_components:
+                if quaternion_component.name == 'identity':
+                    self.orientation['rotation'][0] = quaternion_component.float_value
+                elif quaternion_component.name == 'i':
+                    self.orientation['rotation'][1] = quaternion_component.float_value
+                elif quaternion_component.name == 'j':
+                    self.orientation['rotation'][2] = quaternion_component.float_value
+                elif quaternion_component.name == 'k':
+                    self.orientation['rotation'][3] = quaternion_component.float_value
+        except KeyError:
+            force_set = True
+            pass
+        if orientation is not None or force_set:
+            if self.orientation != orientation or force_set:
+                if orientation is None:
+                    orientation = self.orientation
+                self.orientation = orientation
+        else:
+            self._apply_orientation(self.orientation)
 
     def _update_orientation(self):
         rotation = Rotation(euler_angles_convention=self.coordinate_system.euler_angles_convention['title'])
@@ -380,7 +380,7 @@ class SuperpositionField(Sample, SuperposedField):
         origin = self.coordinate_system.origin
         orientation = {'origin': origin,
                        'rotation': quaternion}
-        self.set_orientation(orientation)
+        self.orientation = orientation
 
     def _apply_orientation(self, orientation):
         rotation = Rotation(quadruple=orientation['rotation'],
