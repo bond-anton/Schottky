c69538dbeaa6260cac3fa9a37a911b670dc19034
bond_anton <bond.anton@gmail.com>
2019-01-26 02:32:22 +0300

    Trap visualisation demo

---
+++ demo/Trap_demo.py
@@ -0,0 +1,51 @@
+from __future__ import division, print_function
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+from BDMesh.Mesh1DUniform import Mesh1DUniform
+from BDMesh.TreeMesh1DUniform import TreeMesh1DUniform
+
+from Schottky.Dopant import Dopant
+from Schottky.Semiconductor import Semiconductor
+from Schottky.Reference import database
+from Schottky import constant
+
+from Schottky.Visual.Trap import draw_bands_diagram
+
+
+reference = database[0]
+
+silicon = Semiconductor('Si', reference)
+
+c_p = Mesh1DUniform(0.0, 5e-6, physical_step=1e-6)
+f_p = Mesh1DUniform(0.0, 5e-6, physical_step=1e-6)
+c_p.solution = np.ones(c_p.num) * 9e20
+f_p.solution = np.zeros(f_p.num)
+phosphorus = Dopant('P', True, TreeMesh1DUniform(c_p, aligned=True), TreeMesh1DUniform(f_p, aligned=True),
+                    0.045 * constant.q, silicon.band_gap_t(0.0) - 0.045 * constant.q,
+                    1e-15, 1e-15)
+phosphorus.charge_state = {0: +1, 1: 0}
+phosphorus.color = 'b'
+phosphorus.linestyle = '--'
+
+c_b = Mesh1DUniform(0.0, 5e-6, physical_step=1e-6)
+f_b = Mesh1DUniform(0.0, 5e-6, physical_step=1e-6)
+c_b.solution = np.ones(c_b.num) * 1e21
+f_b.solution = np.zeros(f_b.num)
+boron = Dopant('B', False, TreeMesh1DUniform(c_b, aligned=True), TreeMesh1DUniform(f_b, aligned=True),
+               silicon.band_gap_t(0.0) - 0.045 * constant.q,  0.045 * constant.q,
+               1e-15, 1e-15)
+boron.charge_state = {0: 0, 1: -1}
+boron.capture_barrier_ev = {0: 0.6, 1: 0.0}
+boron.color = 'r'
+boron.linestyle = '--'
+
+silicon.dopants = [phosphorus, boron]
+
+temperature = np.linspace(1.0, 600.0, num=200, endpoint=True)
+
+ax1 = draw_bands_diagram(phosphorus)
+ax2 = draw_bands_diagram(boron)
+
+plt.show()
