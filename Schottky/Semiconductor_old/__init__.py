31674aa876ec55158edfc4c1c0cbe750d7cc3be1
bond_anton <bond.anton@gmail.com>
2019-09-25 14:02:23 +0300

    Removed old Semiconductor code

--- Schottky/Semiconductor_old/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Semiconductor import Semiconductor
-#from .Trap import Trap
-#from .Dopant import Dopant
-#from .Dislocation import Dislocation
-#from .BondingInterface import BondingInterface
-
